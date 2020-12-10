1e8114ccb8dfc8bc52051f18398fd64926982395
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-12-10 12:26:40 -0500

    deleted spreadsheet data

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -4,12 +4,10 @@ import styled from "styled-components"
 import Layout from "../components/layout"
 import Loading from "../components/Loading"
 import Menu from "../components/Menu/"
-import config from "../components/Menu/config"
 import logo from "../images/logo-landing.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons"
-import { getCarryOut, getDrinks, load, mapItems } from "../utils/spreadsheet"
-import { gapi } from "gapi-script"
+
 import { navigate } from "gatsby"
 
 const Wrap = styled.div`
@@ -27,10 +25,6 @@ const MenuPage = props => {
     headerExpanded: true,
   })
 
-  const setMenuData = data => {
-    console.log(data)
-  }
-
   const expandHeader = () => {
     setData(prev => {
       return { ...prev, headerExpanded: true }
@@ -47,43 +41,6 @@ const MenuPage = props => {
       return { ...prev, bothSectionsClosed: bool }
     })
   }
-  // const onLoad = (packet, error, callback) => {
-  //   console.log("onload ", packet)
-  //   let new_data = data
-  //   if (!packet) {
-  //     console.log("error", error)
-  //     setLoading({ loading: true, message: "failed to load" })
-  //   } else {
-  //     new_data[packet.section] = mapItems(packet)
-  //     setData({ ...data, ...new_data })
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   setLoading({ loading: true, message: "getting menu data..." })
-  //   gapi.load("client:auth2", () => {
-  //     // 2. Initialize the JavaScript client library.
-  //     gapi.client
-  //       .init({
-  //         apiKey: config.apiKey,
-  //         clientId: config.CLIENT_ID,
-  //         discoveryDocs: config.discoveryDocs,
-  //         scope: config.SCOPES,
-  //         // Your API key will be automatically added to the Discovery Document URLs.
-  //       })
-  //       .then(() => {
-  //         // 3. Initialize and make the API request.
-  //         getCarryOut(onLoad)
-  //         getDrinks(onLoad)
-  //       })
-  //   })
-  // }, [])
-
-  // useEffect(() => {
-  //   if (data.Drinks.length > 0 && data.Carryout.length > 0) {
-  //     setLoading({ loading: false, message: "" })
-  //   }
-  // }, [data.Drinks, data.Carryout])
 
   useEffect(() => {
     const interval = setTimeout(() => {
@@ -100,9 +57,6 @@ const MenuPage = props => {
     }
   }, [])
 
-  // function handleAuthClick(event) {
-  //   gapi.auth2.getAuthInstance().signIn()
-  // }
   const scrollAndOpen = () => {
     collapseMenu(true)
     const new_data = { ...data }
--- src/utils/spreadsheet.js
+++ src/utils/spreadsheet.js
@@ -1,65 +1,65 @@
-import config from "../components/Menu/config"
-import { gapi } from "gapi-script"
-import Menu from "../pages/menu"
-export function getCarryOut(callback) {
-  gapi.client.load("sheets", "v4", () => {
-    gapi.client.sheets.spreadsheets.values
-      .get({
-        spreadsheetId: config.spreadsheetId,
-        range: "CARRYOUT!A2:G60",
-      })
-      .then(
-        res => {
-          callback({ section: "Carryout", data: res.result.values }, null, null)
-        },
-        res => {
-          callback(false, res.result.error)
-        }
-      )
-  })
-}
-export function getDrinks(callback) {
-  gapi.client.load("sheets", "v4", () => {
-    gapi.client.sheets.spreadsheets.values
-      .get({
-        spreadsheetId: config.spreadsheetId,
-        range: "DRINKS!A2:G60",
-      })
-      .then(
-        res => {
-          callback({ section: "Drinks", data: res.result.values }, null, null)
-        },
-        res => {
-          callback(false, res.result.error)
-        }
-      )
-  })
-}
+// import config from "../components/Menu/config"
+// import { gapi } from "gapi-script"
+// import Menu from "../pages/menu"
+// export function getCarryOut(callback) {
+//   gapi.client.load("sheets", "v4", () => {
+//     gapi.client.sheets.spreadsheets.values
+//       .get({
+//         spreadsheetId: config.spreadsheetId,
+//         range: "CARRYOUT!A2:G60",
+//       })
+//       .then(
+//         res => {
+//           callback({ section: "Carryout", data: res.result.values }, null, null)
+//         },
+//         res => {
+//           callback(false, res.result.error)
+//         }
+//       )
+//   })
+// }
+// export function getDrinks(callback) {
+//   gapi.client.load("sheets", "v4", () => {
+//     gapi.client.sheets.spreadsheets.values
+//       .get({
+//         spreadsheetId: config.spreadsheetId,
+//         range: "DRINKS!A2:G60",
+//       })
+//       .then(
+//         res => {
+//           callback({ section: "Drinks", data: res.result.values }, null, null)
+//         },
+//         res => {
+//           callback(false, res.result.error)
+//         }
+//       )
+//   })
+// }
 
-export const mapItems = packet => {
-  let menuMap = []
+// export const mapItems = packet => {
+//   let menuMap = []
 
-  packet.data.map(item => {
-    //this means it is a title section
-    if (item[0] !== "") {
-      menuMap.push({
-        section: packet.section || "",
-        type: "title",
-        title: item[0] || "item title",
-        description: item[3] || "",
-      })
-      //this checks for if its a menu item
-    } else if (item[6] === "TRUE") {
-      menuMap.push({
-        section: packet.section || "",
-        type: "item",
-        title: item[1] || "",
-        dietaryNotes: item[2].split(","),
-        description: item[3] || "",
-        price: item[4] || "",
-        additionalNotes: item[5] || "",
-      })
-    }
-  })
-  return menuMap
-}
+//   packet.data.map(item => {
+//     //this means it is a title section
+//     if (item[0] !== "") {
+//       menuMap.push({
+//         section: packet.section || "",
+//         type: "title",
+//         title: item[0] || "item title",
+//         description: item[3] || "",
+//       })
+//       //this checks for if its a menu item
+//     } else if (item[6] === "TRUE") {
+//       menuMap.push({
+//         section: packet.section || "",
+//         type: "item",
+//         title: item[1] || "",
+//         dietaryNotes: item[2].split(","),
+//         description: item[3] || "",
+//         price: item[4] || "",
+//         additionalNotes: item[5] || "",
+//       })
+//     }
+//   })
+//   return menuMap
+// }
